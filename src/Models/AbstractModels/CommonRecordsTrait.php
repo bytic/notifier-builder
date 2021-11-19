71d5fcaf6c67df2526b50d8b67858ce7068adbc6
Gabriel Solomon <hello@gabrielsolomon.ro>
2021-11-19 20:19:44 +0200

    add translation traits to records

--- src/Models/AbstractModels/CommonRecordsTrait.php
+++ src/Models/AbstractModels/CommonRecordsTrait.php
@@ -2,12 +2,24 @@
 
 namespace ByTIC\NotifierBuilder\Models\AbstractModels;
 
+use Nip\I18n\Translatable\HasTranslations;
+
 /**
  * Trait CommonRecordsTrait
  * @package ByTIC\NotifierBuilder\Models\AbstractModels
  */
 trait CommonRecordsTrait
 {
+    use HasTranslations;
+
+    /**
+     * @return string
+     */
+    public function getTranslateRoot()
+    {
+        return $this->getController();
+    }
+
     public function getRootNamespace()
     {
         return 'ByTIC\NotifierBuilder\Models\\';
