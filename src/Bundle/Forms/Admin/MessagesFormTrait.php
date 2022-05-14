08aac81d94f766e631c4f4ca92176bee08e061f9
Gabriel Solomon <hello@gabrielsolomon.ro>
2022-05-14 17:11:06 +0300

    refactor: add messages form trait

---
+++ src/Bundle/Forms/Admin/MessagesFormTrait.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ByTIC\NotifierBuilder\Bundle\Forms\Admin;
+
+trait MessagesFormTrait
+{
+    public function init()
+    {
+        parent::init();
+
+        $this->initCommon();
+    }
+
+    protected function initCommon()
+    {
+        $this->addInput('subject', translator()->trans('subject'), true);
+        $this->addTexteditor('content', translator()->trans('content'), true);
+
+        $this->addButton('save', translator()->trans('submit'));
+    }
+}
