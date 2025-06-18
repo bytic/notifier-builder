205d4e4b9d58e939f846e82b05cc2b5ee4929912
Gabriel Solomon <hello@gabrielsolomon.ro>
2025-06-18 10:12:03 +0300

    feat: add new action to get global messages

---
+++ src/Messages/Actions/Find/FindOrCreateMessageForRecipient.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ByTIC\NotifierBuilder\Messages\Actions\Find;
+
+use ByTIC\NotifierBuilder\Models\Recipients\Recipient;
+
+/**
+ *
+ */
+class FindOrCreateMessageForRecipient extends FindOrCreateMessages
+{
+    /**
+     * @param Recipient $recipient
+     * @param $channel
+     * @return self
+     */
+    public static function forRecipient($recipient, $channel = null)
+    {
+        $action = static::for(
+            $recipient->id_topic,
+            $recipient->getRecipient(),
+            $channel
+        );
+        return $action;
+    }
+
+}
